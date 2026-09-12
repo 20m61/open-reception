a0690d5857cec08f301745f6915ae302fc681c69
20m61 <changhwi.chang@gmail.com>
2026-09-12 10:24:19 +0900

    refactor(motion-lab): nest lab under motions

--- src/app/admin/motion-lab/page.tsx
+++
@@ -1,7 +0,0 @@
-import { MotionLabManager } from '@/components/admin/MotionLabManager';
-
-export const dynamic = 'force-dynamic';
-
-export default function AdminMotionLabPage() {
-  return <MotionLabManager />;
-}
